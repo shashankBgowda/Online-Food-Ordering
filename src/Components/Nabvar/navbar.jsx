8364d6e88f0349379c60e80e1b1bfa6625f996aa
shashankBgowda <shashankbg74@gmail.com>
2025-06-01 10:02:56 -0400

    Accoplished Navbar

--- src/Components/Nabvar/navbar.jsx
+++ src/Components/Nabvar/navbar.jsx
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./navbar.css";
 import { assets } from '../../assets/assets';
 
 const Navbar = () => {
+
+    const [menu, setMenu] = useState("Home");
   return (
     <div className='navbar'>
         <img src={assets.logo} alt="logo"/>
         <ul className='navbar-menu'>
-            <li>Home</li>
-            <li>Menu</li>
-            <li>Contact</li>
+            <li onClick={()=>setMenu("Home")} className={menu==="Home"?"active":""}>Home</li>
+            <li onClick={()=>setMenu("Menu")} className={menu==="Menu"? "active": ""}>Menu</li>
+            <li onClick={()=>setMenu("Contact")} className={menu==="Contact"? "active": ""}>Contact</li>
         </ul>
         <div className='navbar-right'>
             <img src={assets.search_icon} alt='search-img'/>
