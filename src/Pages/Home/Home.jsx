c24eba864eacaca8e62638219f5ba83cd340dfad
shashankBgowda <shashankbg74@gmail.com>
2025-06-02 15:17:30 -0400

    Accomplished menu component

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Home.css';
 import Header from '../../Components/Header/Header';
+import ExploreMenu from '../../Components/ExploreMenu/ExploreMenu';
 
 const Home = () => {
+    const [category, setcategory] = useState("All")
   return (
     <div>
         <Header/>
+        <ExploreMenu category={category} setcategory={setcategory}/>
     </div>
   )
 }
